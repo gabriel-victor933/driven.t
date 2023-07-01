cc8c57fe7a3d66e11896aa4b25d590d2d16d525a
gabriel-victor933 <gabrielvictor933@usp.br>
2023-07-01 11:06:55 -0300

    feat: adding type Cep and throw no content error

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -6,12 +6,14 @@ import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enr
 import { exclude } from '@/utils/prisma-utils';
 
 // TODO - Receber o CEP por parâmetro nesta função.
-async function getAddressFromCEP() {
+async function getAddressFromCEP(cep: string) {
 
   // FIXME: está com CEP fixo!
-  const result = await request.get(`${process.env.VIA_CEP_API}/37440000/json/`);
+  const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
+  console.log(result)
 
-  if (!result.data) {
+  if (result.data?.erro == true ||result.status == 400) {
+    console.log("erro")
     throw notFoundError();
   }
 
