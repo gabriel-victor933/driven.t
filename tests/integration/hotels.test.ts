e8845582d0395761014eb7b154c522b20bd09ec9
gabriel-victor933 <gabrielvictor933@usp.br>
2023-07-13 17:36:06 -0300

    feat: adding PAYMENT REQUIRED test

---
+++ tests/integration/hotels.test.ts
@@ -0,0 +1,102 @@
+import httpStatus from 'http-status';
+import supertest from 'supertest';
+import app, { init } from '@/app';
+import { cleanDb, generateValidToken } from '../helpers';
+import faker from '@faker-js/faker';
+import { createUser, 
+    createEnrollmentWithAddress,
+    createTicketType, 
+    createTicket,
+    createEditableTicketType } from '../factories';
+import { prisma } from '@/config';
+
+const server = supertest(app);
+
+
+beforeAll(async () => {
+    await init();
+  });
+  
+beforeEach(async () => {
+    await cleanDb();
+});
+
+
+describe("GET /hotels",()=>{
+
+    it("deve retornar UNAUTHORIZED quando não é enviado nenhum token",async ()=>{
+        const token = faker.lorem.word()
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+
+        expect(res.status).toBe(httpStatus.UNAUTHORIZED)
+    })
+
+    it("deve retornar UNAUTHORIZED quando o token é invalido",async ()=>{
+        const token = faker.lorem.word()
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+
+        expect(res.status).toBe(httpStatus.UNAUTHORIZED)
+    })
+
+    it("deve retornar NOT FOUND quando não existe inscrição",async ()=>{
+        const token = await generateValidToken()
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(httpStatus.NOT_FOUND)
+    })
+
+    it("deve retornar NOT FOUND quando não existe ticket",async ()=>{
+        const user = await createUser()
+        const token = await generateValidToken(user)
+        const enrollment = await createEnrollmentWithAddress(user)
+        const ticketType = await createTicketType()
+
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(httpStatus.NOT_FOUND)
+    })
+
+    it("deve retornar NOT FOUND quando não existe hotel",async ()=>{
+        const user = await createUser()
+        const token = await generateValidToken(user)
+        const enrollment = await createEnrollmentWithAddress(user)
+        const ticketType = await createEditableTicketType(false,true)
+        const ticket = await createTicket(enrollment.id,ticketType.id,"PAID")
+        
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(httpStatus.NOT_FOUND)
+    })
+
+    it("deve retornar PAYMENT REQUIRED quando status=reserved",async () => {
+        const user = await createUser()
+        const token = await generateValidToken(user)
+        const enrollment = await createEnrollmentWithAddress(user)
+        const ticketType = await createEditableTicketType(false,true)
+        const ticket = await createTicket(enrollment.id,ticketType.id,"RESERVED")
+        
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(httpStatus.PAYMENT_REQUIRED)
+    })
+    it("deve retornar PAYMENT REQUIRED quando o evento é remoto",async () => {
+        const user = await createUser()
+        const token = await generateValidToken(user)
+        const enrollment = await createEnrollmentWithAddress(user)
+        const ticketType = await createEditableTicketType(true,false)
+        const ticket = await createTicket(enrollment.id,ticketType.id,"PAID")
+        
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(httpStatus.PAYMENT_REQUIRED)  
+    })
+    it("deve retornar PAYMENT REQUIRED quando o evento não inclui hotel",async () => {
+        const user = await createUser()
+        const token = await generateValidToken(user)
+        const enrollment = await createEnrollmentWithAddress(user)
+        const ticketType = await createEditableTicketType(false,false)
+        const ticket = await createTicket(enrollment.id,ticketType.id,"PAID")
+        
+        const res = await server.get("/hotels").set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(httpStatus.PAYMENT_REQUIRED)
+    })
+})
+
+describe("GET /hotels/:id",()=>{
+
+})
